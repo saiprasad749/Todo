22c1ebdccf86e4be241d3125b8b5b7290f3f060e
Killada Sri Venkata Sai Durga Prasad <ksaidurg@in.ibm.com>
2019-11-18 22:03:55 +0530

    https://github.com/saiprasad749/Todo.git

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/note_list.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:intl/intl.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: NoteList(),
+    );
+  }
+}
